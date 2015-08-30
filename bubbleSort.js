dd32afcf9a46c129396c3a9c8090b706d1dc849e
bbouley <b.bouley@hotmail.co.uk>
2015-08-30 15:27:09 -0600

    added first boilerplate function

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,5 +1,9 @@
-function bubbleSort(arr){
+var testArray = [1,0,2,5,4,3];
 
+function bubbleSort(arr){
+  for (var i = 0; i < testArray.length; i++) {
+    testArray[i] += 0;
+  }
 }
 
 module.exports = bubbleSort;
